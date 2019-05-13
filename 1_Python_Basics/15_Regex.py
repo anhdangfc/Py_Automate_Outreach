548e6d2f9621fa5c6a12486e453971eeacaa83e2
anhdangfc <anh.dang@fundingcircle.com>
2019-05-13 01:51:35 +0100

    Example codes for REGEX

--- 1_Python_Basics/15_Regex.py
+++ 1_Python_Basics/15_Regex.py
@@ -97,15 +97,91 @@ mo5 = laughUnboundPattern.search('And, I: haha'); print(mo5 == None)
 
 
 ## (2) Greedy and Nongreedy Matching
+hahaPattern = re.compile(r'(ha){1,2}?') ## non-greedy add ? (return the shortest version)
+mo4 = hahaPattern.search('And, I: hahaha'); print(mo4.group())
+
 
 ## (3) .findall()
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d') # has no groups
+phoneNumRegex_group = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)') # has groups
+
+phoneNumRegex.findall('Cell: 415-555-9999 Work: 212-555-0000')
+phoneNumRegex_group.findall('Cell: 415-555-9999 Work: 212-555-0000') # with group (return tuples)
+
+
 ## (4) Character Classes
+xmasRegex = re.compile(r'\d+\s\w+') ## \d+: 1+ digits; \s: space; \w+: 1+ letters/words 
+xmasRegex.findall('12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 \\
+swans, 6 geese, 5 rings, 4 birds, 3 hens, 2 doves, 1 partridge')
+
+
 ## (4b) Your own character classes
+vowelRegex = re.compile(r'[aeiouAEIOU]')
+vowelRegex.findall('Robocop eats baby food. BABY FOOD.')
+
+consonantRegex = re.compile(r'[^aeiouAEIOU]') ## ^ after the bracket, search anything but ones in []
+consonantRegex.findall('Robocop eats baby food. BABY FOOD.')
+
+
 ## (5) Caret and $
+wholeStringIsNum = re.compile(r'^\d+$')
+print(wholeStringIsNum.search('1234567890').group())
+print(wholeStringIsNum.search('12345xyz67890') == None)
+print(wholeStringIsNum.search('123 4567890') == None)
+
+
 ## (6) Wildcard
+atRegex = re.compile(r'.at') ## . match any character except for a newline
+atRegex.findall('The cat in the hat sat on the flat mat.')
+
+nameRegex = re.compile(r'First Name: (.*) Last Name: (.*)') ## .* match everything
+mo = nameRegex.search('First Name: Al Last Name: Sweigart')
+print(mo.group(1))
+print(mo.group(2))
+
+greedyRegex = re.compile(r'<.*>') ## .* is greedy, it takes as long as possible
+mo = greedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+nongreedyRegex = re.compile(r'<.*?>') ## add ? for non-grredy
+mo = nongreedyRegex.search('<To serve man> for dinner.>')
+print(mo.group())
+
+noNewlineRegex = re.compile('.*') ## anything except new line
+noNewlineRegex.search('Serve the public trust.\nProtect the innocent. \\
+\nUphold the law.').group() ## everything up to the first \n
+
+newlineRegex = re.compile('.*', re.DOTALL) ## add re.DOTALL, to also include new line
+newlineRegex.search('Serve the public trust.\nProtect the innocent. \\
+
+
 ## (7) Case Insensitive
+robocop = re.compile(r'robocop', re.I) ## IGNORECASE
+robocop.search('Robocop is part man, part machine, all cop.').group()
+robocop.search('ROBOCOP protects the innocent.').group()
+
+
 ## (8) substitue
+namesRegex = re.compile(r'Agent \w+') ## Start with Agent then followed by Words (till space)
+namesRegex.sub('CENSORED', 'Agent Alice gave the secret documents to Agent Bob.')
+
 ## (9) Complex Regexes
+agentNamesRegex = re.compile(r'Agent (\w{2})\w*') ## n letters in group 1
+## \1, \2, \3 to indicate the group in ( )
+agentNamesRegex.sub(r'\1*****', 'Agent Alice told Agent Carol that Agent Eve knew Agent Bob was a double agent.')
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?            # area code
+    (\s|-|\.)?                    # separator
+    \d{3}                         # first 3 digits
+    (\s|-|\.)                     # separator
+    \d{4}                         # last 4 digits
+    (\s*(ext|x|ext.)\s*\d{2,5})?  # extension
+    )''', 
+    re.VERBOSE) ## for complicated regex pattern, add re.VERBOSE to ignore new lines and comments inside the pattern
+
+
 ## (10) re.IGNORECASE, re.DOTALL, re.VERBOSE
+someRegexValue = re.compile('foo', re.IGNORECASE | re.DOTALL | re.VERBOSE)
 
 
